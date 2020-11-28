f583dae139d27cc2648e7879313cd143daba38bc
abdulrawoofali <rawoofabd41@gmail.com>
2020-11-28 18:58:08 +0530

    .

--- src/index.js
+++ src/index.js
@@ -3,10 +3,57 @@ const app = express()
 const bodyParser = require("body-parser");
 const port = 8080
 
+const LOCAL_HOST = "http://localhost:8080";
+
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
 const { connection } = require('./connector')
+
+app.get(`/totalRecovered`,(req,res)=>{
+    let sum =0 ;
+    connection.find().then(stats => {
+        stats.forEach(stat => {
+            sum+=stat.recovered;
+        });
+        res.send({data: {_id: "total", recovered:sum}});
+    });
+    
+})
+
+app.get(`/totalActive`,(req,res)=>{
+    let sum =0 ;
+    connection.find().then(stats => {
+        stats.forEach(stat => {
+            sum+=(stat.infected-stat.recovered);
+        });
+        res.send({data: {_id: "total", active:sum}});
+    });
+    
+})
+
+
+app.get(`/totalDeath`,(req,res)=>{
+    let sum =0 ;
+    connection.find().then(stats => {
+        stats.forEach(stat => {
+            sum+=(stat.death);
+        });
+        res.send({data: {_id: "total", death:sum}});
+    });
+    
+})
+
+app.get(`/hotspotStates`,(req,res)=>{
+    let result ={data:[]}
+    
+
+    
+    
+})
+
+
 
 
 
